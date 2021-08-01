34573c2fe5d0ce10f9e38190b66c63b1d300bc48
Arturo0911 <anegreiross@outlook.com>
2021-08-01 02:44:00 -0500

    new commit

--- data_simulation.py
+++ data_simulation.py
@@ -4,34 +4,71 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from pprint import pprint
 
 
 def init_simulation(file_name: str):
-    dataset= pd.read_csv("data/"+file_name)
+    dataset = pd.read_csv("data/" + file_name)
     return dataset
+
 
 # print(init_simulation("streptococcus_variant.csv"))
 
 def checking_column():
-
     strep = init_simulation("streptococcus_variant.csv")
     stack_names = ["MEDIUM" if x >= 5.0 else "HIGH" for x in strep["ph_acidification"]]
     strep["acid_category"] = np.array(stack_names)
-    
-    strep.to_csv("strep_test.csv", index = False)
+
+    strep.to_csv("strep_test.csv", index=False)
+
 
 checking_column()
 
-def ploting_charts():
+
+def plotting_charts():
     strep = pd.read_csv("strep_test.csv")
 
-    plt.figure(figsize=(12,7))
-    sns.scatterplot(x = "ufc/ml", y = "20_24_hours", hue = "acid_category", data = strep)
+    plt.figure(figsize=(12, 7))
+    sns.scatterplot(x="ufc/ml", y="20_24_hours", hue="acid_category", data=strep)
     plt.show()
 
-ploting_charts()
+
+plotting_charts()
 
 
+class BacterialGrowth:
+    PANDAS_DATAFRAME = pd.core.frame.DataFrame
 
+    def __init__(self):
+        pass
 
+    def make_dataset_for_simulation(self):
+        pass
+
+    @staticmethod
+    def make_categories(fat_milk: float, proteins: float, tritatable_acidity: float):
+
+        if fat_milk >= 3.25 and proteins >= 2.4 and tritatable_acidity >= 0.9:
+            return "Regular yogurt"
+        elif (3.25 > fat_milk > 1.1) and proteins >= 2.4 and tritatable_acidity >= 0.9:
+            return "Low fat yogurt"
+        elif fat_milk <= 1.1 and proteins >= 2.4 and tritatable_acidity >= 0.9:
+            return "Non fat yogurt"
+
+    @staticmethod
+    def create_dataset(num_rows=10000) -> PANDAS_DATAFRAME:
+
+        # this dataset is for regression model
+        general_dataset = pd.DataFrame(data={
+            'titratable_acidity': np.array([float("{0:.3f}".format(uniform(0.95, 1.19))) for _ in range(num_rows)]),
+            'fat_milk_over_100mg_': np.array([float("{0:.4f}".format(uniform(1, 4))) for _ in range(num_rows)]),
+            'pH_sour_milk_initial': np.array([float("{0:.3f}".format(uniform(4.4, 4.6))) for _ in range(num_rows)]),
+            'streptococcus_strain_': np.array([float("{0:.3f}".format(uniform(4.0, 6.0))) for _ in range(num_rows)]),
+            'lactobacillus_strain_': np.array([float("{0:.3f}".format(uniform(4.0, 6.0))) for _ in range(num_rows)]),
+            'minimum_milk_proteins': np.array([float("{0:.3f}".format(uniform(2.4, 2.7))) for _ in range(num_rows)]),
+            "ideal_temperature_c": np.array([float("{0:.3f}".format(uniform(40, 42))) for _ in range(num_rows)]),
+        })
+
+        # that one is for classification model
+        # classification_dataset = pd.DataFrame(data = {})
+
+        return general_dataset
--- nn_module.py
+++ nn_module.py
@@ -1,8 +1,8 @@
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tesorflow.keras import (
+from tensorflow import keras
+from keras import (
     layers,
     models,
     optimizers
@@ -10,27 +10,24 @@ from tesorflow.keras import (
 import matplotlib.pyplot as plt
 
 
-
 class NeuralNet:
-    
-    def __init__(self, num_epochs, learning_rate = 0.001):
 
+    def __init__(self, num_epochs, learning_rate=0.001):
         self.num_epochs = num_epochs
         self.learning = learning_rate
 
     def model_creation(self, size_layers: int, input_data: int):
-
         model = models.Sequential()
-        model.add(layers.Dense(size_layers, activation = "relu", input_shape = (input_data)))
-        model.add(layers.Dense(size_layers, activation = "relu"))
+        model.add(layers.Dense(size_layers, activation="relu", input_shape=(input_data)))
+        model.add(layers.Dense(size_layers, activation="relu"))
         model.add(layers.Dense(1))
 
-        model.compile(optimizers = optimizers.RMSprop(learning_rate = self.learning),
-                loss = "mse", metrics = ["mae"])
+        model.compile(optimizers=optimizers.RMSprop(learning_rate=self.learning),
+                      loss="mse", metrics=["mae"])
 
         return model
-    
-    def model_training():
+
+    def model_training(self):
         pass
 
 
